2aedba23911799068d20ab00be796a36e35778fd
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-05-08 20:21:33 -0500

    Add: longest-substring-with-at-most-k-distinct-characters - submission-0

---
+++ Data Structures & Algorithms/longest-substring-with-at-most-k-distinct-characters/submission-0.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        mapping = defaultdict(int)
+        left = 0
+        ans = 0
+
+        for right in range(len(s)):
+            mapping[s[right]] += 1
+
+            while len(mapping) > k:
+                mapping[s[left]] -= 1
+                if mapping[s[left]] <= 0:
+                    mapping.pop(s[left])
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
+        

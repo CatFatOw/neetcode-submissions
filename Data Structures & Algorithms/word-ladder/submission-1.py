4b0737bfc83a5c6dc35920577f6ae4ed7cbc31fc
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-05-19 09:57:22 -0500

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        letters = set([chr(i) for i in range(ord("a"), ord("z")+1)])
+
+        def bfs(beginWord, endWord, wordList):
+            queue = deque([(beginWord,1)])
+            visited = set([beginWord])
+            word_set = set(wordList)
+
+            while queue:
+                word, mutation = queue.popleft()
+                if word == endWord:
+                    return mutation
+
+                for idx in range(len(word)):
+                    for char in letters:
+                        word_list = list(word)
+                        word_list[idx] = char
+
+                        result = "".join(word_list)
+                        if result in word_set and result not in visited:
+                            visited.add(result)
+                            queue.append((result, mutation+1))
+            return 0
+        return bfs(beginWord, endWord, wordList)
+            
+
+
+
+        

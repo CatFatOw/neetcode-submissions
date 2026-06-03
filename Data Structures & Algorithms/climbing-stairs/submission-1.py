6eb840df0a673d6bdd295fe490ec5db7e5f42097
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-06-02 22:14:01 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+        def dp(i):
+            if i == 0:
+                return 0
+            if i == 1:
+                return 1
+            if i == 2:
+                return 2
+            if i in memo:
+                return memo[i]
+            best = dp(i-1) + dp(i-2)
+            memo[i] = best 
+            return best 
+        return dp(n)
+
+        

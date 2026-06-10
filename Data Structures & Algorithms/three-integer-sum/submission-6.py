99703281c4401dc362815cf496becec8f4220ac1
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-06-09 21:01:14 -0400

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        mapping = defaultdict(list)
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if i != j:
+                    val = -(nums[i] + nums[j])
+                    mapping[val].append([i, j])
+        
+        result = set()
+        for k in range(len(nums)):
+            if nums[k] in mapping:
+                for i, j in mapping[nums[k]]:
+                    if i != k and j != k:
+                        temp = tuple(sorted([nums[i], nums[j], nums[k]]))
+                        result.add(temp)
+        return [list(x) for x in result]
+
+
+        

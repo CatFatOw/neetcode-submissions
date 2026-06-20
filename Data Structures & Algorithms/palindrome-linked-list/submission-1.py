7d0c33e93f413b529c38aa2ac1b9bbd25ebe87ce
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-06-19 22:02:22 -0400

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+
+        rev = []
+        for i in range(len(stack)-1, -1, -1):
+            rev.append(stack[i])
+        return rev == stack
+
+
+        

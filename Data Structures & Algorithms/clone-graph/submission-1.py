f7c5bf778f4f5873d7e2a8ed7f13d12205ab8538
CatFatOw <140449483+CatFatOw@users.noreply.github.com>
2026-08-18 12:49:14 -0500

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque 
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+        
+        
+        mapping = {node:Node(node.val)}
+        def bfs(start):
+            queue = deque([start])
+
+            while queue:
+                node = queue.popleft()
+                for neighbor in node.neighbors:
+                    if neighbor not in mapping:
+                        mapping[neighbor] = Node(neighbor.val)
+                        queue.append(neighbor)
+                    mapping[node].neighbors.append(mapping[neighbor])
+            return mapping
+        
+        mapping = bfs(node)
+        return mapping[node]
+
+
+
+        
+        
